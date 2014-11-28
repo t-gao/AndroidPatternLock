bb8f15ea8e920056c3bf11d04a7924892de6d120
Tony Gao <gaojian000@gmail.com>
2014-11-28 16:18:00 +0800

    create the project

---
+++ src/com/tg/androidpatternlock/LockView.java
@@ -0,0 +1,24 @@
+package com.tg.androidpatternlock;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class LockView extends LinearLayout {
+
+    public LockView(Context context) {
+        super(context);
+        // TODO Auto-generated constructor stub
+    }
+
+    public LockView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        // TODO Auto-generated constructor stub
+    }
+
+    public LockView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        // TODO Auto-generated constructor stub
+    }
+
+}
